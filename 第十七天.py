599309da31a4ddae33a03a0dda917fe17b769991
Crane <78671144+lgjqdwer0@users.noreply.github.com>
2023-12-05 23:07:17 +0800

    a

--- 第十七天.py
+++ 第十七天.py
@@ -13,10 +13,10 @@ print(type (1+1))
 print(type(1.2))
 
 # eval函数将用户输入作为代码处理,可以将字符串转为数字，列表，字典等
-# logs = input("请输入 ")
-# num = eval(logs)
-# print(num)
-# print(type(num))
+logs = input("请输入 ")
+num = eval(logs)
+print(num)
+print(type(num))
 
 for a in range(1,5):
     for s in range(1,5):
@@ -28,9 +28,12 @@ for a in range(1,5):
 # %s：字符串，%d：十进制整数，%f：浮点数
 age = 12
 name = "flora"
-print("我的名字是%s，我今年%d岁了"%(name,age))  #使用 % 符号作为占位符
-print("我的名字是{0}，我今年{1}岁了".format(name,age))  # str.format() 方法
-print(f"我的名字是{name},我今年{age:.2f}岁了")  # f-string 方法
+# 使用 % 符号作为占位符
+print("我的名字是%s，我今年%d岁了"%(name,age))
+# str.format() 方法
+print("我的名字是{0}，我今年{1}岁了".format(name,age))
+# f-string 方法
+print(f"我的名字是{name},我今年{age:.2f}岁了")
 
 # 写入文件,打开再写入
 with open("pip.txt","w") as f:
