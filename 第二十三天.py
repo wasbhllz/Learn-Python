0b01220ce7230c0746c8af60cc52f12811f2896c
Crane <78671144+lgjqdwer0@users.noreply.github.com>
2023-12-08 21:44:45 +0800

    a

--- 第二十三天.py
+++ 第二十三天.py
@@ -1,4 +1,4 @@
-""" ## 学习
+## 学习
 # input()函数,接受用户输入
 message = input('等等,要我怎么称呼你呢?')
 print('你就是'+message+'吗?很高兴认识你!😊')
@@ -42,28 +42,3 @@ if s%10 ==0:
     print('是10的倍数')
 else:
     print('不是10的倍数')
-
-## 学习
-# while(循环)关键字,会一直执行下去,直到满足某个条件为止
-m = 1
-while m <=5:
-    print(m)
-    m +=1
-# 程序退出
-message = '输入Q退出本程序.\n请输入'
-hh = ''
-o = True # 程序运行的开关在由多个事件中的某一件触发:布尔值
-while hh != 'Q':
-    hh = input(message)
-    if hh == 'Q':
-        o= False
-        print('下次再见!')"""
-        
-# break关键字,终止循环语句的执行,跳出当前for和while循环,继续执行下一条语句
-pp = '输入您的旅游地址,\n按Q退出'
-while True:
-    print(input(pp))
-    if pp == 'Q':
-        break
-    else:
-        print('再见!yuhe')
