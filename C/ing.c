b3e95547606a7a972220220240afb02e9e1f342f
cranewww <2488256548@qq.com>
2023-12-13 12:08:17 +0800

    A

---
+++ C/ing.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main() //函数体内需要打分号;
+{
+   int num = 1;
+   printf("先苦后甜才是第 %d 位.\n",num); 
+   system("pause"); // 运行窗口暂停
+   return 0;
+   // getchar()函数运行窗口暂停
+}
+//忘记在预处理文件前打# 
