41e34a03d70653bade9e3332d2029fab09df9ee0
cranewww <2488256548@qq.com>
2023-12-20 16:02:03 +0800

    A

--- C/hh.c
+++ C/hh.c
@@ -1,3 +1,4 @@
+// 12.18
 #include <stdio.h>
 /* extern int num; //声明来自外部的符号
 int main()
@@ -10,7 +11,8 @@ int main()
      return 0;
 }
 
-
+// 12.19
+// 题目:请问，在执行完第7、第8、第9行后，程序的状态分别是什么？
 #include <stdio.h>
 int main(void)
 {
@@ -32,10 +34,10 @@ scanf("%d",&san); //忘记加&了
 int dan = san * 12 * 30;
 printf("你的年龄大概有%d天\n", dan);
 return 0;
-}*/
+}
 
-
-//定义函数时:先声明,再调用,后定义
+// 12.20
+// 定义和调用函数题目1:先声明,再调用,后定义
 void jolly(void);
 void deny(void);
 
@@ -52,4 +54,47 @@ void jolly(void){
 void deny(void){
             printf("Which nobody can deny!");
       }
+
+// 定义和调用函数题目2
+void br(void);
+void ic(void);
+int main(){
+      br();
+      printf(",");
+      ic();
+      ic();
+      br();
+      return 0;}
+void br(void){
+      printf("Brazil, Russia");
+}
+void ic(void){
+      printf("India, China\n");
+}
+
+// 定义和调用函数题目3
+int main(){
+int toes = 10;
+int toes_2 = 10*2;
+int toes_3 = toes * toes;
+printf("toes = %d\ntoes的两倍 = %d\ntoes的平方 = %d",toes,toes_2,toes_3);
+return 0;
+}*/
+
+// 定义和调用函数题目4
+void one_three(void);
+void two(void); 
+int main(){
+printf("starting now:\n");
+one_three();
+two();
+printf("done!");
+return 0;
+}
+void one_three(void){
+printf("one\n");
+}
+void two(void){
+printf("two\nthree\n");
+}
 
