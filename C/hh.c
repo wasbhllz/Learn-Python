96f2a2b760d01f90af86b4705a66f1c36243e1e4
Crane <78671144+lgjqdwer0@users.noreply.github.com>
2023-12-24 00:47:22 +0800

    A

--- C/hh.c
+++ C/hh.c
@@ -154,7 +154,7 @@ int main(){
     printf("\x29"); // 十六进制的转义序列
     return 0;
 }
-*/
+// 数组:按顺序排列的同类型序列
 int main(){
       int arr[] = {1,2,3,4,5};
       int a = 0;
@@ -164,4 +164,9 @@ int main(){
       }
       return 0;
 } 
-
+*/
+//题目
+int main(){
+      printf("     00\n     00\n000000000000\n000000000000\n    0  0\n    0  0");
+      return 0;
+}
