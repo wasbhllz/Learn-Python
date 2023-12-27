abf5443232f8e49db057eb811dc876f43267501b
cranewww <2488256548@qq.com>
2023-12-27 16:45:05 +0800

    A

---
+++ C/day10.c
@@ -0,0 +1,8 @@
+// 12.27
+#include <stdio.h>
+# define WIFE "散兵"
+int main()
+{
+printf("锵锵！%s登场！",WIFE);
+return 0;
+}
---
+++ C/hello.c
@@ -0,0 +1,7 @@
+/* pip install kaggle
+
+C:\Users\Administrator\.kaggle
+
+笔记拉取API:kaggle kernels pull colinmorris/working-with-external-libraries
+
+笔记拉取API模板：kaggle kernels pull [KERNEL] -p /path/to/download -m */
