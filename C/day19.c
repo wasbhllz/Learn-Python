992bb170b6198d19cc17050fd2a53ae45f95e137
cranewww <2488256548@qq.com>
2024-02-27 16:25:40 +0800

    A

--- C/day19.c
+++ C/day19.c
@@ -1,2 +1,18 @@
 // 24.02.27
-/**/
+/*
+# include<stdio.h>
+void  d (int* x,int* y ){
+    int d = *x; //临时存储
+    *x =*y;
+    *y = d;
+}
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d %d\n",&a,&b);
+    printf("%d %d",a,b);
+    d(&a, &b);
+    printf("%d %d",a,b);
+    return 0;
+}
+*/
