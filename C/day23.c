2619b0874c0dc0177cb2864b9f5ba41e99f8e0a9
Crane <78671144+wasbhllz@users.noreply.github.com>
2024-03-04 20:23:50 +0800

    A

--- C/day23.c
+++ C/day23.c
@@ -47,7 +47,7 @@ int main(){
     printf("青蛙跳到第%d层台阶有%d种方法",steps,frog(steps));
     return 0;
 }
-*/
+
 // 半知半解
 // 选择排序:将数字从大到小输出
 # include <stdio.h>
@@ -79,3 +79,49 @@ int main(){
         return 0;
 }
 
+// 三个数组排序
+# include <stdio.h>
+void swap(int*a,int*b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main(){
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%d %d %d",&a, &b, &c);
+    if(a<b)
+        swap(&a, &b);
+        if(a<c)
+            swap(&a, &c);
+            if(b<c)
+                swap(&b, &c);
+            printf("%d %d %d", a, b, c);
+            return 0;
+}
+
+// 打印1~100之内所以3倍数的数字
+# include <stdio.h>
+int main(){
+    int d = 0;
+    for (d = 3; d < 100;d+=3)
+            printf("%d ", d);
+            return 0;
+}
+*/
+// 给定两个数，求最大公约数
+# include <stdio.h>
+int common(int a,int b){
+    int c = 0;
+    for (c = a; c >= 2;--c)
+    if(b%c==0 && a%c ==0)
+        return c;
+}
+int main(){
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    printf("最大公约数是%d",common(a, b));
+    return 0;
+}
