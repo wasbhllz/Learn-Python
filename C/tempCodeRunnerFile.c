f5fd8ef99ea601d766100195e27dfa49c517cdfb
Crane <78671144+wasbhllz@users.noreply.github.com>
2024-03-06 20:42:04 +0800

    A

---
+++ C/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+int main(){
+    int user = 0;
+    while(1){
+        int conmputer = rand() % 3 + 1;
+            srand(time(0));
+            printf("欢迎来到石头、剪刀、布游戏！\n游戏规则是：1——石头，2——剪刀，3——布\n你选择出什么呢？😁\n");
+            int w = scanf("%d", &user);
+                if(w == conmputer)
+                    printf("平局了！\n");
+                    else if(w ==1 && conmputer == 2 || w == 2 && conmputer == 3 || w == 3 && conmputer == 1){
+                        printf("胜利了！😎\n");
+                        break;
+                    }
+                    else
+                    {
+                    printf("你输了啊！😫\n");
+                    break;
+                    }
+                break;
+    }
+    return 0;
+}
