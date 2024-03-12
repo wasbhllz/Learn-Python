d789e15bd84528ebdc2287f8216f81b4c6f0bbb7
cranewww <2488256548@qq.com>
2024-03-12 16:58:30 +0800

    A

--- C/day28.c
+++ C/day28.c
@@ -1,8 +1,18 @@
 // 24.03.12
 // 将输入的字符串逆向打印
-char reverse_string(char*string){
-    char a = 0;
-    scanf("%c",&a);
-    reverse();
+# include <stdio.h>
+# include <string.h>
+char reverse_string(char* string)
+{
+    int len = strlen(string);
+    for(int a = len;0 <= a;a--)
+    {
+        printf("%s",string[a]);
+    }
+}
+int main()
+{
+    char string[] = {0};
+    scanf("%s",&string);
     return 0;
 }
