1b6ac6a5c246bc79f385546bb7bdf9a0b2c5597f
cranewww <2488256548@qq.com>
2024-03-13 16:03:53 +0800

    A

--- C/project/minesweeper/game.c
+++ C/project/minesweeper/game.c
@@ -1,5 +1,61 @@
 #include "game.h"
-char chessboard(){}
-int game(){
-    chessboard();
+char board[ROW][COL] = {0};
+char board_2[ROW][COL] = {0};
+
+void lay_mines(char board[ROW][COL],int row,int col)
+{
+    int s = 0;
+    while (s <= MINE_COUNT)
+    {
+        int a = rand() % 10;
+        int b = rand() % 10;
+        if(0<a && a<row-1 && 0<b && b<col-1)
+        {
+            board[a][b] = '#';
+            s++;
+        }
+    }
+    int i = 0;
+    int j = 0;
+    for(i = 0;i<row;i++)
+    {
+        for(j = 0;j<col;j++)
+        {
+            if(board[i][j]!='#')
+                board[i][j] = '*';
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void explore(char board_2[ROW][COL],int row,int col){
+    int i,j;
+    for(i = 0;i<row;i++)
+    {
+        for(j =0;j<col;j++)
+        {
+            board_2[i][j] = 'o';
+            printf("%c", board_2[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+void user(char board_2[ROW][COL],int row,int col){
+    int x = 0;
+    int y = 0;
+    scanf("%d,%d",&x,&y);
+    board[x][y] = board_2[x][y];
+    if(board[x][y]!='#')
+
+    board_2[x][y] = '*';
+    else
+    printf("游戏结束！");
+}
+void game()
+{
+    srand(time(0));
+    lay_mines(board, ROW, COL);    explore(board,ROW,COL);
+    user(board,ROW,COL);
 }
--- C/project/minesweeper/game.h
+++ C/project/minesweeper/game.h
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#define ROW 11
+#define COL 11
+// 雷的数量
+#define MINE_COUNT 9
 // 开始游戏！
-game();
-// 初始化棋盘
-chessboard();
+void game();
+// 初始化埋雷的棋盘
+void lay_mines(char borad[ROW][COL],int row,int col);
+// 玩家探索范围
+void explore(char borad[ROW][COL],int row,int col);
 // 打印棋盘
 
 // 
