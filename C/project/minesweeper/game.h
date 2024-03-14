4094d06f676fa079f50a672ad723eaf38a599b0b
cranewww <2488256548@qq.com>
2024-03-14 14:09:49 +0800

    A

--- C/project/minesweeper/game.h
+++ C/project/minesweeper/game.h
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define ROW 11
-#define COL 11
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
 // 雷的数量
-#define MINE_COUNT 9
+#define MINE_COUNT 10
 // 开始游戏！
 void game();
 // 初始化埋雷的棋盘
-void lay_mines(char borad[ROW][COL],int row,int col);
+void lay_mines(char board[ROWS][COLS],int row,int col);
 // 玩家探索范围
-void explore(char borad[ROW][COL],int row,int col);
+void explore(char borad[ROWS][COLS], int row, int col, char ret);
 // 打印棋盘
-
+void current_status(char board[ROWS][COLS], int row, int col);
 // 
+void user(char board[ROW][COL], int row, int col);
