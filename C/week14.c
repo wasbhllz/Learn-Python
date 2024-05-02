ff60058b25690d1fcccb13e734189c04967331d7
cranewww <2488256548@qq.com>
2024-05-02 09:04:22 +0800

    A

--- C/week14.c
+++ C/week14.c
@@ -3,5 +3,15 @@
 /**/
 // 
 int main(){
-    return 0;
+    int i = 0; 
+    char j;
+    char arr[50];
+        while(scanf("%s ",&j)==1){
+            arr[i] = j;
+            i++;
+        }
+        return 0;
 }
+
+// 24.05.02
+//不会辗转相除法！
